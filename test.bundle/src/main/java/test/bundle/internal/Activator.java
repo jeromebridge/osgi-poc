8a03e366abcc919a03ff070c0e44d5b6e3af4b42
jeromebridge <jeromebridge@pennassurancesoftware.com>
2014-06-04 22:59:59 -0400

    Working version running Felix with hot deploy while debugging in Eclipse.

---
+++ test.bundle/src/main/java/test/bundle/internal/Activator.java
@@ -0,0 +1,15 @@
+package test.bundle.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+  public void start(BundleContext arg0) throws Exception {
+    System.out.println("Hello  ... fuck yeah2!!!");
+  }
+
+  public void stop(BundleContext arg0) throws Exception {
+    System.out.println("Bye ... yeah!!");
+  }
+}
