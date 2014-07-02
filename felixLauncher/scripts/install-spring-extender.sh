85fbb32ba581292301e62d1336909acda964c427
jeromebridge <jeromebridge@pennassurancesoftware.com>
2014-07-01 23:29:57 -0400

    Changed install spring script to figure out the script path in order to call other scripts relative to the script not the caller.

--- felixLauncher/scripts/install-spring-extender.sh
+++ felixLauncher/scripts/install-spring-extender.sh
@@ -1,6 +1,5 @@
-echo $0
-
-source scripts/common-commands.sh
+script_path=(new java.io.File $0) getParentFile
+source $script_path/common-commands.sh
 
 install mvn:org.slf4j/com.springsource.slf4j.api/1.6.1
 install mvn:org.slf4j/com.springsource.slf4j.simple/1.6.1
