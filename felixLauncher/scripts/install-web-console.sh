e60b6d8eccd14cffe98cf828359c6b02631e23ee
jeromebridge <jeromebridge@pennassurancesoftware.com>
2014-07-02 01:20:58 -0400

    Fixed script to install web console.

--- felixLauncher/scripts/install-web-console.sh
+++ felixLauncher/scripts/install-web-console.sh
@@ -1,6 +1,6 @@
 script_path=(new java.io.File $0) getParentFile
 source $script_path/common-commands.sh
 
-install obr:org.apache.felix.http.jetty | bundle1=split_and_get " " 2
-install obr:org.apache.felix.webconsole | bundle2=split_and_get " " 2
+install obr:org.apache.felix.http.jetty | grep Bundle | bundle1=split_and_get " " 2
+install obr:org.apache.felix.webconsole | grep Bundle | bundle2=split_and_get " " 2
 start $bundle1 $bundle2
