d2ff149102a1a7ed0a89bea114e6820e42d19f0a
jeromebridge <jeromebridge@pennassurancesoftware.com>
2014-07-01 23:21:16 -0400

    Added gosh_profile to run setup script at startup. Added scripts to install Spring Extender. (still need to work out bug with relative path from script)

---
+++ felixLauncher/scripts/common-commands.sh
@@ -0,0 +1,7 @@
+# Can be piped to and will split the first line only based on the delimeter and index of the split string
+# Param 1: Delimeter
+# Param 2: Index To Get
+split_and_get = { var1 = tac --list; var1 = $var1 get 0; var1 = $var1 split $1; ($var1) | var1 = tac --list; var1 = $var1 get $2; $var1 }
+
+# Param 1: Bundle URL
+ias = { install $1 | bundleNumber = split_and_get " " 2; start $bundleNumber }
--- felixLauncher/scripts/install-spring-extender.sh
+++ felixLauncher/scripts/install-spring-extender.sh
@@ -1,13 +1,17 @@
+echo $0
+
+source scripts/common-commands.sh
+
 install mvn:org.slf4j/com.springsource.slf4j.api/1.6.1
 install mvn:org.slf4j/com.springsource.slf4j.simple/1.6.1
-install mvn:org.slf4j/com.springsource.slf4j.org.apache.commons.logging/1.6.1
-install mvn:org.springframework/org.springframework.core/3.2.4.RELEASE
-install mvn:org.springframework/org.springframework.beans/3.2.4.RELEASE
-install mvn:org.springframework/org.springframework.context/3.2.4.RELEASE
-install mvn:org.apache.log4j/com.springsource.org.apache.log4j/1.2.16
-install mvn:net.sourceforge.cglib/com.springsource.net.sf.cglib/2.2.0
-install mvn:org.aopalliance/com.springsource.org.aopalliance/1.0.0
-install mvn:org.springframework/org.springframework.aop/3.2.4.RELEASE
-install mvn:org.springframework.osgi/spring-osgi-extender/1.2.1
-install mvn:org.springframework.osgi/spring-osgi-core/1.2.1
-install mvn:org.springframework.osgi/spring-osgi-io/1.2.1
+ias mvn:org.slf4j/com.springsource.slf4j.org.apache.commons.logging/1.6.1
+ias mvn:org.springframework/org.springframework.core/3.2.4.RELEASE
+ias mvn:org.springframework/org.springframework.beans/3.2.4.RELEASE
+ias mvn:org.springframework/org.springframework.context/3.2.4.RELEASE
+ias mvn:org.apache.log4j/com.springsource.org.apache.log4j/1.2.16
+ias mvn:net.sourceforge.cglib/com.springsource.net.sf.cglib/2.2.0
+ias mvn:org.aopalliance/com.springsource.org.aopalliance/1.0.0
+ias mvn:org.springframework/org.springframework.aop/3.2.4.RELEASE
+ias mvn:org.springframework.osgi/spring-osgi-io/1.2.1
+ias mvn:org.springframework.osgi/spring-osgi-core/1.2.1
+ias mvn:org.springframework.osgi/spring-osgi-extender/1.2.1
