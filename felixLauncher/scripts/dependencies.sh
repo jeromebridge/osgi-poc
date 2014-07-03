6d66c52ccab7f65be6fa3920c16c162a64dd6a61
jeromebridge <jeromebridge@pennassurancesoftware.com>
2014-07-03 00:07:13 -0400

    Successfully running Liquibase with OSGi.  Yeah!!!

--- felixLauncher/scripts/dependencies.sh
+++ felixLauncher/scripts/dependencies.sh
@@ -3,8 +3,18 @@ source $script_path/common-commands.sh
 
 sh $script_path/install-spring.sh
 
-# ias wrap:mvn:org.liquibase/liquibase-core/3.2.0\$Bundle-SymbolicName=Liquibase&Bundle-Version=3.2.0&Export-Package=liquibase.*\;version=\"3.2.0\"
+# Liquibase
+ias mvn:com.pennassurancesoftware.bundles/com.pennassurancesoftware.bundles.snakeyaml/1.13-SNAPSHOT
 ias mvn:org.everit.osgi/org.everit.osgi.liquibase.bundle/3.1.1-v20140507
+
+# ias wrap:mvn:org.liquibase/liquibase-core/3.2.0\$Bundle-SymbolicName=Liquibase&Bundle-Version=3.2.0&Export-Package=liquibase.*\;version=\"3.2.0\"
+# ias mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.commons-collections/3.2.1_3
+# ias mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.commons-lang/2.4_6
+# ias mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.velocity/1.7_6
+# ias mvn:com.pennassurancesoftware.bundles/com.pennassurancesoftware.bundles.liquibase-core/3.2.0-SNAPSHOT
+
+
+# Yaas Dependencies
 ias mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.commons-dbcp/1.4_3
 ias mvn:org.apache.commons/com.springsource.org.apache.commons.pool/1.5.3
 ias mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.junit/4.11_2
