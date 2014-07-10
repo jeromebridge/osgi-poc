62260af35879c599b3700c2ef8ca7c8d139635f6
jeromebridge <jeromebridge@pennassurancesoftware.com>
2014-07-10 10:15:11 -0400

    save changes to yaas.sh

--- felixLauncher/scripts/yaas.sh
+++ felixLauncher/scripts/yaas.sh
@@ -1,7 +1,8 @@
 script_path=(new java.io.File $0) getParentFile
 source $script_path/common-commands.sh
 
-workspace_path=/home/developer/git/yet-another-admin-system
+# workspace_path=/home/developer/git/yet-another-admin-system
+workspace_path=/Users/Administrator/git/yet-another-admin-system
 
 sh $script_path/dependencies.sh
 
