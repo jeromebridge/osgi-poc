8a1d6c0735693c8e3e1a459e4d0618e887caea2f
jeromebridge <jeromebridge@pennassurancesoftware.com>
2014-10-31 16:27:49 -0400

    Working on deployment strategy for Virgo

--- maven-embedded-poc/src/test/java/poc/osgi/maven/embedded/TestAny.java
+++ maven-embedded-poc/src/test/java/poc/osgi/maven/embedded/TestAny.java
@@ -7,15 +7,29 @@ public class TestAny {
 
    private String getBasedir() {
       // return "/home/developer/Workspaces/Workspace - OSGi2";
-      return "/home/developer/git";
+      return "/home/developer/git/osgi-poc";
    }
 
-   @Test
+   // @Test
    public void testExample() throws Exception {
       MavenCli cli = new MavenCli();
       int result = cli.doMain( new String[]{ "compile" },
-            getBasedir() + "/osgi-poc/test.bundle",
+            getBasedir() + "/test.bundle",
             System.out, System.out );
       System.out.println( "result: " + result );
    }
+
+   @Test
+   public void testExample2() throws Exception {
+      MavenCli cli = new MavenCli();
+      int result = cli.doMain( new String[]{ "dependency:resolve" },
+            getBasedir() + "/test.web",
+            System.out, System.out );
+      System.out.println( "result: " + result );
+   }
+   
+   // Given Eclipse Workspace...figure out the location of each maven project
+   // Loop through each project and assemble a list of dependencies
+   // Determine which projects are OSGi bundles
+   // Use RepoIndex to create OBR configured based on Eclipse workspace and Maven Dependencies
 }
