a2ceab0a620aa890c835646ac8127685613b3460
jeromebridge <jeromebridge@pennassurancesoftware.com>
2015-04-03 14:27:19 -0400

    Using workaround that doesn't break other Virgo applications (for now).

--- qi4j.bundle/src/main/java/poc/osgi/qi4j/internal/Activator.java
+++ qi4j.bundle/src/main/java/poc/osgi/qi4j/internal/Activator.java
@@ -1,28 +1,12 @@
-/*
- * Copyright 2008 Niclas Hedhman. All rights Reserved.
- *
- * Licensed  under the  Apache License,  Version 2.0  (the "License");
- * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed  under the  License is distributed on an "AS IS" BASIS,
- * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
- * implied.
- *
- * See the License for the specific language governing permissions and
- * limitations under the License. 
- */
-
 package poc.osgi.qi4j.internal;
 
 import java.util.Hashtable;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.framework.wiring.BundleWiring;
 import org.qi4j.api.structure.Application;
 import org.qi4j.api.structure.Module;
 import org.qi4j.bootstrap.ApplicationAssembler;
@@ -50,27 +34,37 @@ public final class Activator implements BundleActivator {
 
    @SuppressWarnings({ "rawtypes", "unchecked" })
    public void start( BundleContext bundleContext ) throws Exception {
-      //      final Bundle bundle = bundleContext.getBundle();
-      //      final BundleWiring bundleWiring = bundle.adapt( BundleWiring.class );
-      //      final ClassLoader classLoader = bundleWiring.getClassLoader();
+      final Bundle bundle = bundleContext.getBundle();
+      final BundleWiring bundleWiring = bundle.adapt( BundleWiring.class );
+      final ClassLoader classLoader = bundleWiring.getClassLoader();
+      final ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+      System.out.println( "Bundle Classloader: " + classLoader );
+      System.out.println( "Context Classloader: " + contextClassLoader );
 
-      // WORKAROUND: You must do this because of the way ProxyGenerator gets the classloader to create a Proxy class
-      // Thread.currentThread().setContextClassLoader( null ); // This causes web to fail to deploy
+      try {
+         // WORKAROUND: You must do this because of the way ProxyGenerator gets the classloader to create a Proxy class
+         Thread.currentThread().setContextClassLoader( null ); // This causes web to fail to deploy
 
-      LOGGER.info( "Starting Bundle [" + bundleContext.getBundle().getSymbolicName() + "]" );
+         LOGGER.info( "Starting Bundle [" + bundleContext.getBundle().getSymbolicName() + "]" );
 
-      final Energy4Java boot = new Energy4Java();
-      final ApplicationAssembler assembler = getApplicationAssembler( bundleContext );
-      application = boot.newApplication( assembler );
+         final Energy4Java boot = new Energy4Java();
+         final ApplicationAssembler assembler = getApplicationAssembler( bundleContext );
+         application = boot.newApplication( assembler );
 
-      LOGGER.info( "Activating application." );
-      application.activate();
+         LOGGER.info( "Activating application." );
+         application.activate();
 
-      Module module = application.findModule( LAYER_NAME, MODULE_NAME );
-      LOGGER.info( "Find module [" + LAYER_NAME + ", " + MODULE_NAME + "] isFound [" + ( module != null ) + "]" );
+         Module module = application.findModule( LAYER_NAME, MODULE_NAME );
+         LOGGER.info( "Find module [" + LAYER_NAME + ", " + MODULE_NAME + "] isFound [" + ( module != null ) + "]" );
 
-      moduleRegistration = bundleContext.registerService( Module.class.getName(), module, new Hashtable() );
-      LOGGER.info( "Module registered." );
+         moduleRegistration = bundleContext.registerService( Module.class.getName(), module, new Hashtable() );
+         LOGGER.info( "Module registered." );
+      }
+      finally {
+
+         // WORKAROUND: You must do this because of the way ProxyGenerator gets the classloader to create a Proxy class
+         Thread.currentThread().setContextClassLoader( contextClassLoader ); // Hopefully this fixes the issues caused by the workaround above
+      }
    }
 
    private ApplicationAssembler getApplicationAssembler( final BundleContext bundleContext ) {
